1a4ff70dc73c8820002cd95d71920f4c75e4f81c
Alex Racinski <aracinski2@gmail.com>
2020-12-08 20:28:49 -0600

    add button to retrieve data from rails to html

--- products.js
+++ products.js
@@ -3,6 +3,16 @@ var app = new Vue({
   data: function () {
     return {
       message: "Hello from JavaScript!",
+      todos: []
     };
   },
+  methods: {
+    getData: function () {
+      axios.get('http://localhost:3000/api/products').then(response => {
+        console.log('i am line 11');
+        console.log(response);
+        this.todos = response.data;
+      });
+    }
+  }
 });
